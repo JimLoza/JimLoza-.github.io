f4f38fd604d6891a989728f561bb6572d664b5e2
Jim Loza <jimloza25@gmail.com>
2021-03-04 16:07:22 -0600

    cambios para el deploy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&api_key=btBCqvQ1EWpsxkIdmOwmndhkVEp8BE9W&limit=10`;
+    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&api_key=btBCqvQ1EWpsxkIdmOwmndhkVEp8BE9W&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
